c965df1836b1e2e4afbfb22e12b1216fb76b2d48
The Geek on Skates <geekonskates@protonmail.com>
2023-08-29 23:16:41 -0400

    Started on the server changes

--- server/server.php
+++ server/server.php
@@ -1,36 +1,14 @@
 <?php
 
-// It should be noted that when this is not running in localhost, it
-// would be best to put this outside the publicly-accessible web root. :D
-// Just stayin'.  But for now, not an issue.
-$path = ".";
+include_once("setup.php");
 
-// Get the user's token.  If one wasn't passed, create a new file to
-// store stuff, and return the name of that file.
-$token = $_POST['t'];
-if (!isset($token)) {
-	$token = date("YmdHis");
-	file_put_contents("$path/$token.json", "{}");
-	exit($token);
-}
-
-// Make sure there is a file matching the token; note that down the
-// road, I'll have a background job deleting old token files.  Buf for now,
-// I don't really care, so just check if it exists and save it to memory.
-if (!file_exists("$path/$token.json")) exit("Your token has expired.\r\n");
-$data = json_decode(file_get_contents("$path/$token.json"), true);
-
-// Get the user's input
-$input = $_POST['i'];
-if (!isset($input)) exit("Missing input.\n");
-
-// Welcome message
-if ($input == "::welcome::") exit("\r\n\r\n2 days to Friday! :D\r\nUsername:\t");
+// Welcome message - I was watching a pirate flick tonight, so this works
+if ($input == "::welcome::") exit("\r\n\r\nAvast, ye scallywag!\nWho dares to com aboard me ship? :-)\n");
 
 // Exit message
 if ($input == "::end-session::") {
 	unlink("$path/$token.json");
-	exit("Bye.\r\n");
+	exit("alright then, walk the plank!\n\n");
 }
 
 // Obviously this is just for kicks, no SSL or anything (YET), but how
@@ -39,12 +17,15 @@ if (!array_key_exists('user', $data)) {
 	if ($input == "geek") {
 		$data['user'] = "geek";
 		file_put_contents("$path/$token.json", json_encode($data));
-		exit("Okay, you're in!\n");
+		exit("Welcome aboard, matey!\n");
 	}
-	else exit("Username:\t");
+	else exit("Arrrrrg... Password?\t");
 }
 
 // Now we got our command, what do we do with it?  How about something
 // really dumb, something I would never do in serious code! Woooo! :D
-echo shell_exec($input);
+if (preg_match("~^cowsay \".{1,}\"\\\$~", $input) === 0) {
+	exit(shell_exec("/usr/games/$input"));
+}
+exit("Ya scurvy scoundrel!  Ye can't do that!\n");
 
