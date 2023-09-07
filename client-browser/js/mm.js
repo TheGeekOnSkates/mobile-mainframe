e8254d8f67a576dba6b2c447923abba641c24a7e
The Geek on Skates <geekonskates@protonmail.com>
2023-09-06 20:59:47 -0400

    Got it reading my input
    It's not great, but it gets the job done for the initial connection.
    For communicating with the 1.0 server, this might actually be all we need.

--- client-browser/js/mm.js
+++ client-browser/js/mm.js
@@ -1,12 +1,46 @@
+// -------------------------------------------------------------------------
+// GLOBAL VARIABLES
+// -------------------------------------------------------------------------
+
+// term: The terminal; url: the URL you're connected to;
+// on: 1 if connected, 0 if not
+var term, url = "", on = 0;
+
+
+
+// -------------------------------------------------------------------------
+// FUNCTIONS
+// -------------------------------------------------------------------------
+
+function onInput(data) {
+	if (on) {
+		// Send to a WebSocket
+		return;
+	}
+	
+	if (data == "\x7f") {
+		// Backspace
+		if (!url) return;
+		term.write("\x1b[D\x1b[K");
+		url = url.substr(0, url.length - 1);
+		return;
+	}
+	
+	if (data == "\r") {
+		// Enter
+		term.write("\r\n\nGoing to " + url + "\r\n\n");
+		url = "";
+		return;
+	}
+	
+	// Otherwise, add it to the URL :)
+	url += data;
+	term.write(data);
+}
+
 window.onload = function() {
-	var term = new Terminal();
+	term = new Terminal();
 	term.open(terminal);
-	term.write('c[34mTO-DO:[0m\r\n');
-	term.write("\t1. Get this terminal talking to the 1.0 server\r\n");
-	term.write("\t2. Review how to talk to WebSockets from JS\r\n");
-	term.write("\t3. Write a WebSockets server program in PHP\r\n");
-	term.write("\t4. Get this terminal talking to that program\r\n");
-	term.write("\t5. Learn libwebsockets and build the C client\r\n");
-	term.write("\t6. Get the C client talking to the PHP program\r\n");
-	term.write("\t7. Test, polish, WTFM, repeat. :-)\r\n\n\n");
+	term.onData(onInput);
+	term.focus();
 };
