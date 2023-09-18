0a985c4e533ddee8a9bc852d2a477d3e0bd41df1
The Geek on Skates <geekonskates@protonmail.com>
2023-09-17 21:39:55 -0400

    Yes!  Multiple connections! :-)

--- README.md
+++ README.md
@@ -5,16 +5,14 @@ This fun little project is [explained in better detail here](https://thegeekonsk
 
 ## TO-DO's for version 2.0:
 
-1. As I go, keep adding features to the client:
+1. I'm nearly done with the multiple-connection thing.  But it's got bugs:
+	- After one client disconnects, my server goes BANANAS with "supplied resource is not a valid Socket resource".  It makes sense that it would do this about trying to read from a closed connection - but unsetting the thing doesn't seem to help (and sometimes the error is triggered by `socket_select`, which makes no sense except to my God and maybe The Inventor Of The Internet). 😆
+	- Once that's fixed, the other thing I want to make sure I do is to make sure all child processes are closed and all pipes are closed and all sockets are closed, and... yeah, when it shuts down, it needs to SHUT THE PUCK DOWN. 😆
+2. As I go, keep adding features to the client:
 	- Better key input processing for the URL-reading code in `onInput`
 	- Maybe a disconnect shortcut/button?
 	- Make sure my changes don't break the 1.0 support :-)
-2. Get the script to support multiple connections, and lock it down
-	- From what I've read, the multiple connection thing is done with `socket_select` (I'm sure the PHP docs will show how it's done)
-	- When a user disconnects, have the client ask for an address again
-	- If the user closes the process opened with `proc_open`, disconnect him/her.  This way there is not room for doing other stuff... like kinda makes me wonder what is possible if I hit :q in vim...
 3. Write that chat app - I can do this in any language now!  Maybe C/ncurses
 4. Once it seems the web version is good enough to call done... which in and of itself would be *amazing*... build the terminal client (using C and libwebsockets).
 5. Test, polish, WTFM, push to master, call it done-for-tnow, and start on the stuff I wanted to use this for! :-)
-
 
--- server/server2.php
+++ server/server2.php
@@ -1,5 +1,18 @@
 <?php
 
+// ------------------------------------------------------------------------
+// SETTINGS
+// ------------------------------------------------------------------------
+
+$command = "vim";
+
+
+
+// ------------------------------------------------------------------------
+// SERVER CODE STARTS HERE
+// ------------------------------------------------------------------------
+
+
 include_once("./ws.php");
 
 // Let the script hang around waiting for connections
@@ -9,52 +22,89 @@ set_time_limit(0);
 $server = WS_CreateServer("localhost", 12345);
 if (is_null($server)) exit("Error creating server");
 
-// Create the client socket
-$client = WS_CreateClient($server);
-if (is_null($client)) exit("Error creating client");
+// Create an array to store client sockets for socket_select purposes
+$clients = array($server);
 
-socket_set_nonblock($client);
+// And this array will store the client sockets AND associated processes
+$users = array();
 
-
-// Still experimenting with passing data to/from command-line apps
-$descriptorspec = array(
-   0 => array("pipe", "r"),  // stdin
-   1 => array("pipe", "w"),  // stdout
-   2 => array("pipe", "w"),  // stderr
-);
-$cwd = '/var/www/html/mm';
-$process = proc_open('stdbuf -o0 /usr/bin/vim 2>&1', $descriptorspec, $pipes, $cwd);
-
-stream_set_blocking($pipes[0], 0);
-stream_set_blocking($pipes[1], 0);
-stream_set_blocking($pipes[2], 0);
-
-// Send messages into WebSocket in a loop.
 while (true) {
-	$output = fgetc($pipes[1]);
-	if ($output !== false) WS_Write($client, $output);
+    // Copy the array of client sockets
+    $read = array_values($clients);
+
+    // Check for changes in the socket status
+    socket_select($read, $write, $except, 0);
 	
-	// Is this not sending to $pipes[0]?
-	$input = "";
-    $input = socket_read($client, 1024);
-	if ($input !== false) {
-		$input = WS_Translate($input);
-		fputs($pipes[0], $input);
+    // Handle new client connections
+    if (in_array($server, $read)) {
+        $newSocket = WS_CreateClient($server);
+		if (is_null($newSocket)) {
+			error_log("WS_CreateClient failed");	// socket_strerror...
+		} else {
+			socket_set_nonblock($newSocket);
+			$clients[] = $newSocket;
+			$index = array_search($server, $read);
+			unset($read[$index]);
+			
+			// Run a command-line app in a separate process; when the client
+			// connects, PHP will just be a sort of "middle-man", relaying
+			// info between this child process and the client.
+			$descriptorspec = array(
+			   0 => array("pipe", "r"),  // stdin
+			   1 => array("pipe", "w"),  // stdout
+			   2 => array("pipe", "w"),  // stderr
+			);
+			$process = proc_open("stdbuf -o0 $command 2>&1",
+				$descriptorspec, $pipes);
+			stream_set_blocking($pipes[0], 0);
+			stream_set_blocking($pipes[1], 0);
+			stream_set_blocking($pipes[2], 0);
+			
+			// Push the new client and precess into the $users array
+			array_push($users, array(
+				"socket" => $newSocket, "process" => $process,
+				"stdin" => $pipes[0], "stdout" => $pipes[1],
+				"stderr" => $pipes[2]
+			));
+		}
 	}
 	
-	/*
-	// Send $input to gnuchess
-	fwrite($pipes[0], $input);
-
-	// Read output from gnuchess and send it to the client
-	WS_Write($client, stream_get_contents($pipes[1]));
-	*/
-	
-	/*
-	// Old code (worked great)
-    sleep(1);
-	WS_Write($client, "\033c\033[34mTime: \033[33m" . date("H:i:s"));
-	*/
+	// Handle each client's I/O stuff
+	foreach($users as &$user) {
+		// If the socket is NULL, skip it
+		if (is_null($user["socket"])) continue;
+		
+		// If the process is no longer running, disconnect the user
+		$status = proc_get_status($user["process"]);
+		if (!$status["running"]) {
+			socket_close($user["socket"]);
+			// $user["socket"] = NULL;
+			// Also gotta close the pipes and process if not open.
+			unset($user);
+			continue;
+		}
+		
+		// Get output from the process, if there is any,
+		// and if so send it to the client
+		$output = fgetc($user["stdout"]);
+		if ($output !== false) WS_Write($user["socket"], $output);
+		
+		// Read input from the client, and if there is any,
+		// send it to the process
+		$input = "";
+		$input = socket_read($user["socket"], 1024);
+        if ($input === false) {
+			// Client disconnected - Do I need to socket_close?
+			//socket_close($user["socket"]);
+			// $user["socket"] = NULL;
+			// Also gotta close the pipes and process if not open.
+			unset($user);
+			continue;
+		}
+		if ($input == "") continue;
+		$input = WS_Translate($input);
+		fputs($user["stdin"], $input);
+	}
 }
 
-// End of test server program
+// End of server program
